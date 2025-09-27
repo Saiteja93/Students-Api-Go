b6f4f0a3ec35a17c02f20c57e02c6de94530dde8
Saiteja93 <saitejaraj.guvvala@gmail.com>
2025-09-27 16:46:33 -0700

    commit

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,54 @@
-package main
+package config
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
 
-func main() {
-    fmt.Println("Hello from Students API project!")
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+// Httpserver holds server-specific config
+type Httpserver struct {
+	Addr string `yaml:"addr" env:"HTTP_ADDR" `
+}
+
+// Config is the root application config
+type Config struct {
+	Env         string     `yaml:"env" env:"ENV" env-required:"true"`
+	Storagepath string     `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
+	Httpserver  Httpserver `yaml:"http_server"`
+}
+
+// MustLoad loads config from ENV or file and exits on failure
+func MustLoad() *Config {
+	var configpath string
+
+	// 1. Check environment variable
+	configpath = os.Getenv("CONFIG_PATH")
+
+	// 2. If not set, check CLI flag
+	if configpath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configpath = *flags
+		if configpath == "" {
+			log.Fatal("config path is not set (use CONFIG_PATH env var or --config flag)")
+		}
+	}
+
+	// 3. Ensure the file exists
+	if _, err := os.Stat(configpath); os.IsNotExist(err) {
+		log.Fatalf("config file doesn't exist: %s", configpath)
+	}
+
+	// 4. Parse the config
+	var cfg Config
+	err := cleanenv.ReadConfig(configpath, &cfg)
+	if err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
+	}
+
+	return &cfg
 }
